6333b6796956fda35242fa267603d16e9df6b254
Rafael Delforge <rafadevvv@gmail.com>
2022-01-08 19:39:40 -0300

    Version Mobile

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,23 @@
+const checkbox = document.getElementById("checkbox-menu");
+const menuMobile = document.getElementById("menu-mobile");
+
 setTimeout(() => {
     document.getElementsByClassName("none")[0].style = "opacity: 0;"
     document.getElementsByClassName("none")[1].style = "opacity: 0;"
-}, 5000)
+}, 1000)
 
 setTimeout(() => {
     document.getElementsByClassName("none")[0].style = "display: none;"
     document.getElementsByClassName("none")[1].style = "display: none;"
-}, 5500)
+}, 1500)
+
+checkbox.addEventListener("click", () => {
+    
+    if (checkbox.checked) {
+        menuMobile.style.display = "block";
+        document.body.style.overflow = "hidden";
+    } else {
+        menuMobile.style.display = "none";
+        document.body.style.overflow = "visible";
+    }
+})
