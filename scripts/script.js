b0ed2435099efafdc4831a5f99cad02c7a95a251
Rafael Delforge <rafadevvv@gmail.com>
2022-01-12 20:54:00 -0300

    Responsividade adicionada - cores ainda ruins

--- scripts/script.js
+++ scripts/script.js
@@ -4,20 +4,24 @@ const menuMobile = document.getElementById("menu-mobile");
 setTimeout(() => {
     document.getElementsByClassName("none")[0].style = "opacity: 0;"
     document.getElementsByClassName("none")[1].style = "opacity: 0;"
-}, 5000)
+}, 1000)
 
 setTimeout(() => {
     document.getElementsByClassName("none")[0].style = "display: none;"
     document.getElementsByClassName("none")[1].style = "display: none;"
-}, 5500)
+}, 1500)
 
 checkbox.addEventListener("click", () => {
     
     if (checkbox.checked) {
-        menuMobile.style.display = "block";
+        // menuMobile.style.display = "block";
+        menuMobile.style.top = "0px";
         document.body.style.overflow = "hidden";
     } else {
-        menuMobile.style.display = "none";
+        // menuMobile.style.display = "none"
+        menuMobile.style.top = "-700px"
         document.body.style.overflow = "visible";
     }
+
+    // document.body.scrollTop = 10000;
 })
